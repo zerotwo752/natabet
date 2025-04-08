0616f1a2be5ac2e363c164336c0084aef2ab7327
zerotwo752 <71127773@continental.edu.pe>
2025-04-07 20:06:02 -0500

    1_Equipos.py

--- pages/1_Equipos.py
+++ pages/1_Equipos.py
@@ -27,7 +27,7 @@ st.markdown("""
     
     /* Sidebar */
     [data-testid="stSidebar"] {
-        background-color: #2d2d2d  !important;
+        background-color: #2A2A2A  !important;
     }
     
     /* Mensajes de información */
