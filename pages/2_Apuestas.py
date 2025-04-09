786be6a9ac2a69d08753afc5dd601306b1e04bdf
zerotwo752 <71127773@continental.edu.pe>
2025-04-09 02:09:27 -0500

    2_Apuestas.py

--- pages/2_Apuestas.py
+++ pages/2_Apuestas.py
@@ -440,7 +440,7 @@ def display_team(team_name, team_members):
         <meta charset="utf-8">
         <style>
           .team-container {{
-              width: 1000px;
+              width: 800px;
               margin: 20px auto;
               padding: 20px;
               background-color: #272752;
