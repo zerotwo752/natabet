3050bed7d31beb4dae896b4554782db5be3a2255
zerotwo752 <71127773@continental.edu.pe>
2025-04-15 21:07:27 -0500

    1_Equipos.py

--- pages/1_Equipos.py
+++ pages/1_Equipos.py
@@ -87,18 +87,11 @@ st.markdown(f"""
         margin-bottom: 5px;
         font-size: 16px;
     }}
-    .mmr-difference {
-    font-size: 24px;
-    color: #FFFFFF;
-    font-weight: bold;
-    text-align: center;
-    text-shadow:
-        -1px -1px 0 purple,
-         1px -1px 0 purple,
-        -1px  1px 0 purple,
-         1px  1px 0 purple;
-}
-
+    .mmr-difference {{
+        font-size: 24px;
+        color: #FFFFFF;
+        font-weight: bold;
+        text-align: center;
     }}
     .title {{
         font-size: 32px;
